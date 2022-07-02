17190228f03f666cac940768a11f7adae70a8103
sonyaxent <104161452+sonyaxent@users.noreply.github.com>
2022-07-02 12:09:01 +0200

    Пересдача_Вахнован
    Ничего не работает, но я все еще пытаюсь разобраться:)

--- peresdacha.py
+++ peresdacha.py
@@ -1,29 +1,60 @@
 import functools
-from collections import Counter
+import sys
+import time
+
+import requests
+from memory_profiler import memory_usage
+
+
+def profile_memory(msg='Memory info'):
+    def internal(f):
+        @functools.wraps(f)
+        def deco(*args, **kwargs):
+            result = f(*args, **kwargs)
+            result_to_memorize = tuple(result)
+            mem = memory_usage(proc=result_to_memorize)
+            print(msg, f'({f.__name__}):  {mem}')
+            return result
+
+        return deco
+
+    return internal
+
+
+def cache_lfu(max_limit=100):
+    def internal(f):
+        @functools.wraps(f)
+        def deco(*args, **kwargs):
+            cache_key = (args, tuple(kwargs.items()))
+            deco._cache[cache_key]["counter"] = 1
+            if cache_key in deco._cache:
+                deco._cache[cache_key]["counter"] += 1
+                return deco._cache[cache_key]
+
+            result = f(*args, **kwargs)
+
+            if len(deco._cache) >= max_limit:
+                del deco._cache[min(deco._cache, key=lambda cache_key: deco._cache[cache_key]["counter"])]
+            deco._cache[cache_key]["result"] = result
+            return result
+        deco._cache = {}
+        return deco
+    return internal
 
 
 
-def cache(f, maxsize=100):
-    @functools.wraps(f)
-    def deco(*args):
-        if args in deco._cache:
-            counter = Counter()
-            for key, value in deco_cache.items():
-                counter[key] = 0
-                for v in value:
-                    if v in value:
-                        counter[key] += 1
-                        to_delete = min(counter.values())
-                        if len(deco._cache) >= maxsize:
-                            deco._cache.pop(to_delete)
-            return deco._cache[args]
-        result = f(*args)
-        deco._cache[args] = result
-        return result
-    deco._cache = {}
-    return deco
+@cache_lfu(max_limit=30)
+@profile_memory
+def fetch_url(url, first_n=120):
+    """Fetch a given url"""
+    res = requests.get(url)
+    return res.content[:first_n] if first_n else res.content
 
 
-
-
-
+fetch_url('https://google.com')
+fetch_url('https://google.com')
+fetch_url('https://google.com')
+fetch_url('https://ithillel.ua')
+fetch_url('https://dou.ua')
+fetch_url('https://ain.ua')
+fetch_url('https://youtube.com')
